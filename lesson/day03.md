8c4bee5c8856018be8ce30f82c4eed654df914eb
yuhu <13343589996@163.com>
2021-07-06 00:16:00 +0800

    controller and data

---
+++ lesson/day03.md
@@ -0,0 +1,7 @@
+# 数据mock
+- 创建服务 www.js
+- 处理路由 app.js
+- 匹配路由 (router)
+- 处理数据（controller）
+- promise 处理postData
+
--- src/router/blog.js
+++ src/router/blog.js
@@ -1,31 +1,38 @@
+const {getList,getDetail,newBlog,updateBlog} = require('../controller/blog')
+const {SuccessModel,ErrorModel} = require('../model/resModel')
 const handleBlogRouter = (req,res) => {
-
+ 
   // 获取博客列表
   let {method,path} = req;
+  let id = req.query.id
+
   if(method === 'GET' && path === '/api/blog/list'){
-    return {
-      msg:'这是获取博客列表的接口'
-    }
+    let keyword = req.query.keyword || ''
+    let author = req.query.author || ''
+    let listData = getList(author,keyword)
+    return new SuccessModel(listData)
   }
 
   //获取博客详情
   if(method === 'GET' && path === '/api/blog/detail'){
-    return {
-      msg:'这是获取博客详情的接口'
-    }
+    let data = getDetail(id)
+    return new SuccessModel(data)
+
   }
 
   //新建一篇博客 
   if(method === 'POST' && path === '/api/blog/new'){
-    return {
-      msg:'这是新建一篇博客的接口'
-    }
+    const data = newBlog(req.body);
+    return new SuccessModel(data)
   }
 
   // 更新一篇博客 
   if(method === 'POST' && path === '/api/blog/update'){
-    return {
-      msg:'这是更新一篇博客的接口'
+    const result = updateBlog(id,req.body)
+    if(result) {
+      return new SuccessModel()
+    }else{
+      return new SuccessModel('更新博客失败')
     }
   }
   // 删除一篇博客
